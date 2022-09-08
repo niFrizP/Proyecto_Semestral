6bab77fa3d1c222f1ebcf9eb035e20a19cc22abd
Nico FP <ni.friz@duocuc.cl>
2022-09-08 15:00:18 -0400

    cambios en home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage{
 
-  constructor(private activatedRouter:ActivatedRoute, private router:Router) { }
+username:string='';
 
-  ngOnInit() {
+  constructor(
+    private activatedRouter:ActivatedRoute, 
+    private router:Router,
+    private menu:MenuController,
+  ) {
+
+    this.activatedRouter.queryParams.subscribe(params=>{
+      if(this.router.getCurrentNavigation().extras.state){
+
+        let usuario=this.router.getCurrentNavigation().extras.state.miusuario;
+        console.log("Llego el state: " + usuario.username);
+        this.username=usuario.username;
+      }
+      
+    })
+
+
+  }
+
+  verMenu(){
+    this.menu.open('first')
   }
 
 }
